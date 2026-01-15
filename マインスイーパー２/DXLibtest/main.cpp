6632e0833c53af2c0729f31d860745ec363e8cd4
MSI\GA2A <h1202415006@hiratagakuen.onmicrosoft.com>
2026-01-15 12:31:35 +0900

    タイマー表示の続きをやる

--- マインスイーパー２/DXLibtest/main.cpp
+++ マインスイーパー２/DXLibtest/main.cpp
@@ -5,14 +5,24 @@ const int VAR        = 12;//縦
 const int HOR        = 15;//横
 const int MINE_COUNT = 30;
 
+// 地雷マップ
+class Land
+{
+public:
+	void Mine(int SafeCol, int SafeRow);
+	void Num();
+
+	int Mine_map[HOR][VAR];
+};
+
 // 描画マップ
 class Map
 {
 public:
-	void Open_Cell(int row, int col);//最初は芝生マス→地雷マップの情報を得て反映
+	void Open_Cell(int row, int col, int data);//最初は芝生マス→地雷マップの情報を得て反映
+	void Open_Area(int col, int row, Land& land);
+
 	int Cell[HOR][VAR];
-private:
-	
 };
 
 class Image
@@ -20,10 +30,9 @@ class Image
 public:
 	void Load();
 	void Draw(const Map& map);
-	~Image();
 
 private:
-	int gh_back; //背景
+	int gh_back; 
 	int gh_turf;
 	int frag;
 	int mine;
@@ -37,19 +46,9 @@ private:
 	int n_seven;
 };
 
-// 地雷マップ
-class Land
-{
-public:
-	void Mine();
-	void Num();
-	void Space();
-	int Mine_map[HOR][VAR];
-private:
 
-};
 
-void Land::Mine()
+void Land::Mine(int SafeCol, int SafeRow)
 {
 	for (int i = 0; i < VAR; i++)
 	{
@@ -64,6 +63,9 @@ void Land::Mine()
 	{
 		int x = rand() % HOR;
 		int y = rand() % VAR;
+
+		if (abs(x - SafeCol) <= 1 && abs(y - SafeRow) <= 1)
+			continue;
 
 		if (Mine_map[x][y] == 0)
 		{	
@@ -102,9 +104,38 @@ void Land::Num()
 	}
 }
 
-void Map::Open_Cell(int row, int col)
+void Map::Open_Cell(int row, int col, int data)
 {
-	Cell[row][col] = 0;//Empty
+	Cell[row][col] = data;
+}
+
+void Map::Open_Area(int col, int row, Land& land)
+{
+	// 範囲外
+	if ((col < 0 || col >= HOR || row < 0 || row >= VAR) || (Cell[col][row] != -2))
+	{
+		return;
+	}
+
+	// マスを開く
+	Cell[col][row] = land.Mine_map[col][row];
+
+	// 0 でなければ終了
+	if (Cell[col][row] != 0)
+		return;
+
+	// 8方向
+	for (int x = -1; x <= 1; x++)
+	{
+		for (int y = -1; y <= 1; y++)
+		{
+			if (x == 0 && y == 0)
+			{
+				continue;
+			}
+			Open_Area(col + x, row + y, land);
+		}
+	}
 }
 
 void Image::Load()
@@ -125,39 +156,62 @@ void Image::Load()
 
 void Image::Draw(const Map & map)
 {
-	if (gh_back != -1)
-	{
-		DrawGraph(0, 0, gh_back, TRUE);
-	}
+	DrawGraph(0, 0, gh_back, TRUE);
+	DrawGraph(0, 13, frag, TRUE);
 
-	if ((gh_turf != -1) && (empty != -1))
+	for (int i = 0; i < VAR; i++)
 	{
-		for (int i = 0; i < VAR; i++)
+		for (int j = 0; j < HOR; j++)
 		{
-			for (int j = 0; j < HOR; j++)
+			int x = j * 32;
+			int y = MARGIN + (i * 32);
+			int value = map.Cell[j][i];
+
+			if (value == -2)
 			{
-				if (map.Cell[j][i] == 0)
-				{
-					DrawGraph((j * 32), MARGIN + (i * 32), empty, TRUE);
-				}
-				else 
-				{
-					DrawGraph((j * 32), MARGIN + (i * 32), gh_turf, TRUE);
-				}
+				DrawGraph(x, y, gh_turf, TRUE);
+			}
+			else if (value == -1)
+			{
+				DrawGraph(x, y, mine, TRUE);
+			}
+			else if (value == 0)
+			{
+				DrawGraph(x, y, empty, TRUE);
+			}
+			else if (value == -3)
+			{
+				DrawGraph(x, y, frag, TRUE);
+			}
+			else if (value == 1)
+			{
+				DrawGraph(x, y, n_one, TRUE);
+			}
+			else if (value == 2)
+			{
+				DrawGraph(x, y, n_two, TRUE);
+			}
+			else if (value == 3)
+			{
+				DrawGraph(x, y, n_three, TRUE);
+			}
+			else if (value == 4)
+			{
+				DrawGraph(x, y, n_four, TRUE);
+			}
+			else if (value == 5)
+			{
+				DrawGraph(x, y, n_five, TRUE);
+			}
+			else if (value == 6)
+			{
+				DrawGraph(x, y, n_six, TRUE);
+			}
+			else if (value == 7)
+			{
+				DrawGraph(x, y, n_seven, TRUE);
 			}
 		}
-	}
-	
-}
-Image::~Image()
-{
-	if (gh_back != -1)
-	{
-		DeleteGraph(gh_back);
-	}
-	if (gh_turf != -1)
-	{
-		DeleteGraph(gh_turf);
 	}
 }
 
@@ -177,13 +231,25 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance,
 
 	int mouseX, mouseY;//カーソル位置保存用
 	int prevMouseInput = 0;// 前のフレームのときのマウス状態
+	int Remaining = MINE_COUNT;
+	int StartTimer = 0;
+
+	bool GameOver = false;
+	bool FirstClick = true;
 
 	Image Box;
 	Map map;
 	Land land;
 
 	Box.Load();
-	land.Mine();
+
+	for (int i = 0; i < VAR; i++)
+	{
+		for (int j = 0; j < HOR; j++)
+		{
+			map.Cell[j][i] = -2; // 未開封
+		}
+	}
 
 	while (1) 
 	{
@@ -194,21 +260,67 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance,
 		GetMousePoint(&mouseX, &mouseY);
 
 		int mouseInput = GetMouseInput();
+		int row = (mouseY - MARGIN) / 32;
+		int col = mouseX / 32;
+		int data;
 
-		if ((mouseInput & MOUSE_INPUT_LEFT) && !(prevMouseInput & MOUSE_INPUT_LEFT))
+		if ((mouseInput & MOUSE_INPUT_LEFT) && !(prevMouseInput & MOUSE_INPUT_LEFT) && (GameOver == false))
 		{
-			int row = (mouseY - MARGIN) / 32;
-			int col = mouseX / 32;
-
-			if ((0 <= row && row < 12) && (0 <= col && col < 15))
+			if ((0 <= row && row < VAR) && (0 <= col && col < HOR)) 
 			{
-				map.Open_Cell(col, row);
+				if (FirstClick)
+				{
+					land.Mine(col, row);
+					land.Num();
+					FirstClick = false;
+
+					// タイマースタート
+					StartTimer = GetNowCount();
+
+				}
+				if (map.Cell[col][row] == -2)
+				{
+					data = land.Mine_map[col][row];
+
+					if (data == -1)
+					{
+						map.Open_Cell(col, row, data);
+						GameOver = true;
+					}
+					else if (data == 0)
+					{
+						map.Open_Area(col, row, land);
+					}
+					else
+					{
+						map.Open_Cell(col, row, data);
+					}
+				}
 			}
 		}
 
+		if ((mouseInput & MOUSE_INPUT_RIGHT) && !(prevMouseInput & MOUSE_INPUT_RIGHT) && (GameOver == false))
+		{
+			if ((0 <= row && row < VAR) && (0 <= col && col < HOR))
+			{
+				if (map.Cell[col][row] == -2)
+				{
+					map.Cell[col][row] = -3; // 旗を置く
+					Remaining--;
+				}
+				else if (map.Cell[col][row] == -3)
+				{
+					map.Cell[col][row] = -2; // 旗を外す
+					Remaining++;
+				}
+			}
+		}
 		prevMouseInput = mouseInput;
 
 		Box.Draw(map);
+
+		SetFontSize(30);
+		DrawFormatString(40, 15, GetColor(255, 255, 255), "%d", Remaining);
 
 		//--------------------------------------------------------------------
 
